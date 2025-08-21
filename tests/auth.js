5baa4a7c44bd78ba418e1464678668db3ebeeac4
Matthew Broadley <matthew@firstpinelands.org>
2025-08-21 15:52:50 +0200

    feature: add safe persistence flow

---
+++ tests/auth.js
@@ -0,0 +1,28 @@
+import { chromium } from '@playwright/test';
+
+const USER_DATA_DIR = '.auth/.wa-profile';
+
+(async () => {
+  const ctx = await chromium.launchPersistentContext(USER_DATA_DIR, {
+    headless: false, viewport: null,
+  });
+  try {
+    const page = await ctx.newPage();
+    await page.goto('https://web.whatsapp.com');
+    const loggedIn = !!await page.getByRole('button', { name: 'Chats' }).isVisible();
+    if (loggedIn) {
+      console.log("Already logged in.");
+    } else {
+      console.log("Please scan QR code...");
+      await page.getByRole('button', { name: 'Chats' }).waitFor({ timeout: 60000 });
+      console.log("Logged in.");
+      const welcomeModal = !!await page.getByText('A fresh look').isVisible();
+      if (welcomeModal) {
+        await page.getByRole('button', { name: 'Continue' }).click();
+        console.log("Closed welcome modal.");
+      }
+    }
+  } finally {
+    await ctx.close();
+  }
+})();
--- tests/send.spec.js
+++ tests/send.spec.js
@@ -1,26 +1,27 @@
 // @ts-check
-import { test, expect } from '@playwright/test';
+import { test, expect, chromium } from '@playwright/test';
 import { list, body } from '../list.js';
 import 'dotenv/config';
 
-test('send', async ({ page }) => {  
+// QR auth and persist context
+const USER_DATA_DIR = '.auth/.wa-profile';
+
+test('Send messages', async () => {
+  const ctx = await chromium.launchPersistentContext(USER_DATA_DIR, {
+    headless: false
+  });
+  const page = await ctx.newPage();
   await page.goto('/');
-  await expect(page.getByText('wa-logowa-wordmark')).toBeVisible();
-  await expect(page.getByRole('img', { name: 'Scan this QR code to link a' })).toBeVisible();
-  await page.getByText('A fresh look').waitFor({ timeout: 120000 }); // Manually Scan QR code
-  await page.getByRole('button', { name: 'Continue' }).click();
+  await expect(page.getByRole('button', { name: 'Chats' })).toBeVisible();
   
   // Loop and send
+  await page.pause();
   for (let i = 0; i < list.length; i++) {
-    console.log(i);
     await page.getByRole('textbox', { name: 'Search input textbox' }).fill(list[i].name);
-    await page.getByTitle(list[i].name, { exact: true}).click();
+    await page.getByTitle(list[i].name, { exact: true }).click();
     await page.getByRole('textbox', { name: 'Type a message' }).fill(body(list[i].name, list[i].message));
     await page.getByRole('button', { name: 'Send' }).click();
-    await page.getByLabel('Pending').waitFor({ state: 'hidden'});
-    console.log(list[i]);
+    await page.getByLabel('Pending').waitFor({ state: 'hidden' });
   }
-  await page.close();
+  await ctx.close();
 });
-
-
